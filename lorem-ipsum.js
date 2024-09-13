1120bcda734c5dfce4de733585fd3e760008b841
David Awogbemila <awogbemila@chromium.org>
2024-09-13 14:53:49 -0400

    Add animation.progress demo required-page reading demo

---
+++ lorem-ipsum.js
@@ -0,0 +1,17 @@
+let lorem_ipsum = "";
+
+function initLoremIpsum(n=21) {
+  const lorem_ipsum_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim\
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in\
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla\
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in\
+                    culpa qui officia deserunt mollit anim id est laborum.";
+  while(n--) {
+    lorem_ipsum += lorem_ipsum_text;
+  }
+}
+function fillSection(section) {
+  section.innerText += lorem_ipsum;
+}
